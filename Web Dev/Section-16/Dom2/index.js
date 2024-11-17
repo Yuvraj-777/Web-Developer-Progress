69a88070f1a1cb76953cfeedbfbc3017b9f56146
Yuvraj <83761474+Yuvraj-777@users.noreply.github.com>
2024-11-18 00:28:43 +0530

    Add files via upload

---
+++ Web Dev/Section-16/Dom2/index.js
@@ -0,0 +1,5 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Yuvraj";
+document.getElementsByTagName("li")[2].style.color = "purple";
+document.querySelector("h1").innerHTML = "GoodBye";
+document.querySelector("li a").style.color = "green";
+document.querySelector("li").style.color = "red";
---
+++ Web Dev/Section-16/Dom3/index.js
@@ -0,0 +1,9 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Yuvraj";
+document.getElementsByTagName("li")[2].style.color = "purple";
+document.querySelector("li a").style.color = "green";
+document.querySelector("li").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML = "<em>Good Night</em>";
